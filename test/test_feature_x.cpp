0f8649377862e77a0464935eb17fa7bd61c73bc7
Erich Schroeter <erichschroeter@gmail.com>
2021-05-06 20:50:59 -0500

    Added example of Catch2 not working well with CMake generator expressions

---
+++ test/test_feature_x.cpp
@@ -0,0 +1,7 @@
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE( "testing generator expressions with FEATURE_X" )
+{
+	REQUIRE( true );
+}
---
+++ test/test_feature_y_1.cpp
@@ -0,0 +1,7 @@
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE( "testing generator expressions with FEATURE_Y 1" )
+{
+	REQUIRE( true );
+}
